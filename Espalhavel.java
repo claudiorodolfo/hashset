f29d9d59d9cd5388f0dd99c61912e1d7b616d3fa
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2021-08-08 15:54:10 -0300

    Update Espalhavel.java

--- Espalhavel.java
+++ Espalhavel.java
@@ -4,9 +4,9 @@ public interface Espalhavel {
 	//remove(), remove o  objeto com a chave especificada
 	public Object remover(String chave);
 	//contains(), consulta se um determinado valor existe na tabela
-	//public boolean contemValor(String chave);
+	//public boolean contemValor(Object dado);
 	//containsKey(), consulta se uma determinada chave existe na tabela
-	public boolean contemChave(String chave);	
+	public boolean contemChave(String chave);
 	//get(), retorna o objeto  associado a chave especificada
 	public Object buscar(String chave);
 	//getAll(), retorna todos os objetos armazenados
